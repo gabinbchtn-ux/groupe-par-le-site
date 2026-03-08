a5c6cc9521a0a522d7db7b494c9b8d171ccb64b9
gabinbchtn-ux <gabinbchtn@gmail.com>
2026-03-08 22:53:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
 ------------------------------------------------- */
 const firebaseConfig = {
   // 👉 Remplacez ces valeurs par celles de votre projet Firebase
-  apiKey: "VOTRE_API_KEY",
-  authDomain: "VOTRE_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://VOTRE_PROJECT_ID.firebaseio.com",
-  projectId: "VOTRE_PROJECT_ID",
-  storageBucket: "VOTRE_PROJECT_ID.appspot.com",
-  messagingSenderId: "VOTRE_SENDER_ID",
-  appId: "VOTRE_APP_ID"
+  apiKey: "AIzaSyCFFI_TOzVlWX1GCAZW4tsx-Z80qqgkXpM",
+  authDomain: "partage-e313d.firebaseapp.com",
+  databaseURL: "https://partage-e313d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "partage-e313d",
+  storageBucket: "partage-e313d.firebasestorage.app",
+  messagingSenderId: "815247760270",
+  appId: "1:815247760270:web:f079ba6b1a8e22439462df"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -258,3 +258,4 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 }); // ← fin DOMContentLoaded
+
